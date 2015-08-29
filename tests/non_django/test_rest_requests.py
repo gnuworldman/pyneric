0a4fdcb8a3684c1a56bf72c6701894340e9e1109
Craig Hurd-Rindy <gnuworldman@gmail.com>
2015-08-29 16:19:36 -0700

    Comply with standards (whitespace, naming, no logical changes).

--- tests/non_django/test_rest_requests.py
+++ tests/non_django/test_rest_requests.py
@@ -40,33 +40,39 @@ class RestResourceTestCase(TestCase):
     def test_init_container_url(self):
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
+
         obj = Resource(ROOT)
         self.assertEqual('{}/{}'.format(ROOT, Resource.url_path), obj.url)
 
     def test_init_container_url_trailing_slash(self):
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
+
         obj = Resource(ROOT + '/')
         self.assertEqual('{}/{}/'.format(ROOT, Resource.url_path), obj.url)
 
     def test_init_container_url_and_path_trailing_slash(self):
         class Resource(rest_requests.RestResource):
             url_path = 'resource/'
+
         obj = Resource(ROOT + '/')
         self.assertEqual('{}/{}'.format(ROOT, Resource.url_path), obj.url)
 
     def test_init_container_url_invalid(self):
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
+
         for bad_container in (Resource, 123, set()):
             self.assertRaises(ValueError, Resource, bad_container)
 
     def test_init_container_resource(self):
         class Container(rest_requests.RestResource):
             url_path = 'container'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         obj = Resource(Container(ROOT))
         expected = '{}/{}/{}'.format(ROOT, Container.url_path,
                                      Resource.url_path)
@@ -75,9 +81,11 @@ class RestResourceTestCase(TestCase):
     def test_init_container_resource_trailing_slash(self):
         class Container(rest_requests.RestResource):
             url_path = 'container/'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         obj = Resource(Container(ROOT))
         expected = '{}/{}{}/'.format(ROOT, Container.url_path,
                                      Resource.url_path)
@@ -86,38 +94,46 @@ class RestResourceTestCase(TestCase):
     def test_init_container_resource_invalid(self):
         class Container(rest_requests.RestResource):
             url_path = 'container'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         for bad_container in (ROOT, Container, 'abc', 123, set()):
             self.assertRaises(ValueError, Resource, bad_container)
 
     def test_init_base_container_attribute_resolution(self):
         class Container(rest_requests.RestResource):
             url_path = 'container'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         container = Container(ROOT)
         obj = Resource(container)
         self.assertEqual(obj.container_, container)
         self.assertFalse(hasattr(obj, 'container__'))
+
         # again with "container_" existing
-        class Resource(rest_requests.RestResource):
+        class Resource1(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
             container_ = 'blah'
-        obj = Resource(container)
+
+        obj = Resource1(container)
         self.assertEqual(obj.container__, container)
-        self.assertFalse(hasattr(Resource(Container(ROOT)), 'container___'))
+        self.assertFalse(hasattr(Resource1(Container(ROOT)), 'container___'))
 
     def test_init_container_reference_attribute(self):
         class Container(rest_requests.RestResource):
             url_path = 'container'
             reference_attribute = 'the_container'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         container = Container(ROOT)
         obj = Resource(container)
         self.assertEqual(obj.container, container)
@@ -136,9 +152,11 @@ class RestResourceTestCase(TestCase):
     def test_from_url(self):
         class Container(rest_requests.RestResource):
             url_path = 'container'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         url = '{}/{}/{}'.format(ROOT, Container.url_path, Resource.url_path)
         obj = Resource.from_url(url)
         self.assertIsInstance(obj.container_, Container)
@@ -147,14 +165,17 @@ class RestResourceTestCase(TestCase):
     def test_from_url_invalid(self):
         class Container(rest_requests.RestResource):
             url_path = 'container'
+
         class Resource(rest_requests.RestResource):
             url_path = 'resource'
             container_class = Container
+
         self.assertRaises(ValueError, Resource.from_url, 'invalid://url')
 
     def test_methods_with_or_without_url_parameter(self):
         class Resource(rest_requests.RestResource):
             url_path = 'a'
+
         obj = Resource(ROOT)
         # These are valid, pass-through methods.
         for attr in ('get', 'post', 'head', 'options'):
@@ -167,8 +188,10 @@ class RestResourceTestCase(TestCase):
     def test_metadata(self):
         class Resource0(rest_requests.RestResource):
             url_path = 'a'
+
         class Resource1(rest_requests.RestResource):
             url_path = 'b'
+
         self.assertEqual('a', Resource0.url_path)
         self.assertEqual('b', Resource1.url_path)
 
@@ -178,15 +201,17 @@ class RestCollectionTestCase(TestCase):
     def test_init_collection(self):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
+
         url = '{}/{}'.format(ROOT, Collection.url_path)
         self.assertEqual(url, Collection(ROOT).url)
 
     def test_init_member(self):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
-        id = 'important thing'
-        url = '{}/{}/{}'.format(ROOT, Collection.url_path, id)
-        self.assertEqual(url, Collection(ROOT, id).url)
+
+        id_ = 'important thing'
+        url = '{}/{}/{}'.format(ROOT, Collection.url_path, id_)
+        self.assertEqual(url, Collection(ROOT, id_).url)
 
     def test_init_invalid_id_type(self):
         bases = (rest_requests.RestCollection,)
@@ -197,19 +222,24 @@ class RestCollectionTestCase(TestCase):
         class CustomType(object):
             def __init__(self, value):
                 super(CustomType, self).__init__()
+
             def __str__(self):
                 return ''
+
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
             id_type = CustomType
+
         self.assertRaises(ValueError, Collection, ROOT, 'test')
 
     def test_from_url_member(self):
         class Container(rest_requests.RestCollection):
             url_path = 'things'
+
         class Resource(rest_requests.RestCollection):
             url_path = 'others'
             container_class = Container
+
         container_id = 'a thing'
         resource_id = 'another'
         url = '{}/{}/{}/{}/{}'.format(ROOT, Container.url_path, container_id,
@@ -224,29 +254,33 @@ class RestCollectionTestCase(TestCase):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
             id_type = int
-        id = 2112
-        url = '{}/{}/{}'.format(ROOT, Collection.url_path, id)
-        self.assertEqual(url, Collection(ROOT, id).url)
+
+        id_ = 2112
+        url = '{}/{}/{}'.format(ROOT, Collection.url_path, id_)
+        self.assertEqual(url, Collection(ROOT, id_).url)
 
     def test_from_url_member_invalid_type(self):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
             id_type = int
-        id = 'not_an_integer'
-        url = '{}/{}/{}'.format(ROOT, Collection.url_path, id)
+
+        id_ = 'not_an_integer'
+        url = '{}/{}/{}'.format(ROOT, Collection.url_path, id_)
         self.assertRaises(ValueError, Collection.from_url, url)
 
     def test_from_url_ambiguous(self):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
-        id = 'things'
+
+        id_ = 'things'
         interpreted_root = '{}/{}'.format(ROOT, Collection.url_path)
-        url = '{}/{}'.format(interpreted_root, id)
+        url = '{}/{}'.format(interpreted_root, id_)
         self.assertRaises(ValueError, Collection.from_url, url)
 
     def test_from_url_unambiguous_collection(self):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
+
         url = '{}/{}'.format(ROOT, Collection.url_path)
         obj = Collection.from_url(url)
         self.assertIsNone(obj.id)
@@ -256,9 +290,10 @@ class RestCollectionTestCase(TestCase):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
             id_type = int
-        id = 'things'
+
+        id_ = 'things'
         interpreted_root = '{}/{}'.format(ROOT, Collection.url_path)
-        url = '{}/{}'.format(interpreted_root, id)
+        url = '{}/{}'.format(interpreted_root, id_)
         obj = Collection.from_url(url)
         self.assertIsNone(obj.id)
         self.assertEqual(interpreted_root, obj.container)
@@ -267,6 +302,7 @@ class RestCollectionTestCase(TestCase):
         class Collection(rest_requests.RestCollection):
             url_path = 'things'
             id_type = int
+
         obj = Collection(ROOT)
         self.assertEqual(Collection.is_abstract, obj.is_abstract)
         self.assertEqual(Collection.url_path, obj.url_path)
@@ -287,6 +323,7 @@ class RestCallTestCase(TestCase):
     def test_get_httpbin_user_agent(self):
         class UserAgent(rest_requests.RestResource):
             url_path = 'user-agent'
+
         user_agent = 'Mozilla, but not really'
         headers = {'User-Agent': user_agent,
                    'Accept': 'application/json'}
